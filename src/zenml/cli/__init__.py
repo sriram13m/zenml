7da2fd4dfeff7ef6877e9332e1edc6fcd5f43754
Michael Schuster <michael.schuster.ffb@googlemail.com>
2022-03-28 13:28:24 +0200

    Add implementation of local/sql stack store with user management

--- src/zenml/cli/__init__.py
+++ src/zenml/cli/__init__.py
@@ -352,6 +352,7 @@ from zenml.cli.base import *  # noqa
 from zenml.cli.config import *  # noqa
 from zenml.cli.example import *  # noqa
 from zenml.cli.integration import *  # noqa
+from zenml.cli.permission_management import *  # noqa
 from zenml.cli.pipeline import *  # noqa
 from zenml.cli.stack import *  # noqa
 from zenml.cli.stack_components import *  # noqa
